ee1c9ec63445fc1c2eee7ad8cf6be42007b33fdc
Valeriia Lytvynova <lyt.valery3340@gmail.com>
2022-09-10 16:48:11 +0300

    Add folder cleaner

---
+++ folder_cleaner/main.py
@@ -0,0 +1,58 @@
+import sys
+from shutil import unpack_archive
+from pathlib import Path
+import folder_sorter as parser
+from name_normalizer import normalize
+
+
+def handle_files(file_name: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    normalized_name = normalize(normalize(file_name.stem)) + file_name.suffix
+    print(f'...replacing {file_name.name}')
+    file_name.replace(target_folder / normalized_name)
+
+
+def handle_archives(file_name: Path, target_folder: Path):
+    target_folder.mkdir(exist_ok=True, parents=True)
+    name = normalize(file_name.stem)
+    normalized_name = name + file_name.suffix
+    full_name = target_folder / normalized_name
+    file_name.replace(full_name)
+    target_folder.mkdir(exist_ok=True, parents=True)
+    new_dir = target_folder / name
+    new_dir.mkdir(exist_ok=True, parents=True)
+    print(f'...unpacking archive {file_name.name}')
+    unpack_archive(full_name, new_dir)
+
+
+def handle_folder(file_name: Path):
+    print(f'...deleting {file_name.name}')
+    file_name.rmdir()
+
+
+def main(folder: Path):
+    parser.scan_folder(folder)
+    for file in parser.IMAGES:
+        handle_files(file, folder / 'images')
+    for file in parser.VIDEO:
+        handle_files(file, folder / 'video')
+    for file in parser.AUDIO:
+        handle_files(file, folder / 'audio')
+    for file in parser.DOCUMENTS:
+        handle_files(file, folder / 'documents')
+    for file in parser.OTHERS:
+        handle_files(file, folder / 'others')
+    for file in parser.ARCHIVES:
+        handle_archives(file, folder / 'archives')
+    for file in parser.FOLDERS:
+        handle_folder(file)
+    parser.print_lst()
+
+
+if __name__ == '__main__':
+    if sys.argv[1]:
+        folder_for_scan = Path(sys.argv[1])
+        print(f'Work with "{folder_for_scan}" folder...')
+        main(folder_for_scan.resolve())
+    else:
+        print('Not enough arguments')
--- folder_cleaner/name_normalizer.py
+++ folder_cleaner/name_normalizer.py
@@ -1,2 +1,26 @@
+import re
 
 
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ('a', 'b', 'v', 'g', 'd', 'e', 'e', 'j', 'z', 'i', 'j', 'k', 'l',
+               'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'ts', 'ch',
+               'sh', 'sch', '', 'y', '', 'e', 'yu', 'u', 'ja', 'je', 'ji', 'g')
+
+TRANS = {}
+
+for cyrylic, trans in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(cyrylic)] = trans
+    TRANS[ord(cyrylic.upper())] = trans.upper()
+
+
+def translate(file_name: str):
+    translate_name = ''
+    for char in file_name:
+        translate_name += TRANS.get(ord(char), char)
+    return translate_name
+
+
+def normalize(ukr_string: str):
+    eng_string = ukr_string.translate(TRANS)
+    eng_string = re.sub(r"\W", "_", eng_string)
+    return eng_string
