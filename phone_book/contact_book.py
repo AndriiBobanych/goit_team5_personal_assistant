155c24f1400209b4c76c59db2bac872578b32893
Andrii Bobanych <andrii.bobanych@gmail.com>
2022-09-10 13:23:45 +0300

    Commit: main classes for contact-book
    Was added classes
    - Field (and Name Phone Email Address Birthday)
    - Record (with __repr__)
    - ContactBook (with methods to add contact and save book)

---
+++ phone_book/contact_book.py
@@ -0,0 +1,38 @@
+from collections import UserDict
+import pickle
+
+from field import Field, Name, Phone, Email, Address, Birthday
+from record import Record
+
+
+class ContactBook(UserDict):
+    __book_name = "contact_book.pickle"
+
+    def __enter__(self):
+        self.__update()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.__save()
+
+    def __update(self):
+        try:
+            with open(self.__book_name, "rb+") as file:
+                book = pickle.load(file)
+                self.data.update(book)
+        except Exception:
+            return "Contact-book is not updated"
+
+    def __save(self):
+        try:
+            with open(self.__book_name, "wb+") as file:
+                pickle.dump(self.data, file, protocol=pickle.HIGHEST_PROTOCOL)
+                print("Contact-book was successfully saved")
+        except Exception:
+            print("Some problems arose during saving")
+
+    def add_new_contact(self, name: Name, phone: Phone = None, email: Email = None,
+                        address: Address = None, birthday: Birthday = None):
+        new_contact = Record(name=name, phone=phone, email=email, address=address, birthday=birthday)
+        self.data[name.value] = new_contact
+
--- phone_book/record.py
+++ phone_book/record.py
@@ -1,2 +1,20 @@
+from field import Field, Name, Phone, Email, Address, Birthday
 
+
+class Record:
+
+    def __init__(self, name: Name, phone: Phone = None, email: Email = None,
+                 address: Address = None, birthday: Birthday = None):
+        self.name = name
+        self.phone = [phone] if phone is not None else []
+        self.email = [email] if email is not None else []
+        self.address = [address] if address is not None else []
+        self.birthday = birthday if birthday is not None else ""
+
+    def __repr__(self):
+        return f"name: {self.name.value}; " \
+               f"phones: {' '.join(phone.value for phone in self.phone)}; " \
+               f"emails: {' '.join(email.value for email in self.email)}; " \
+               f"addresses: {' '.join(address.value for address in self.address)}; " \
+               f"birthday: {self.birthday.value if self.birthday is not None else ''}"
 
