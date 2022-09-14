4dbb3e6b4fbc58ba1cf2a8d512e4265aa1c65312
Andrii Bobanych <andrii.bobanych@gmail.com>
2022-09-15 01:24:19 +0300

    initial commit (created files)

--- team5_personal_assistant/main_note_book.py
+++ team5_personal_assistant/main_note_book.py
@@ -2,10 +2,131 @@ import functools
 import os
 from pathlib import Path
 
-try:
-    from class_note_book import *
-except:
-    from .class_note_book import *
+# try:
+#     from class_note_book import *
+# except:
+#     from .class_note_book import *
+
+
+# -------------------------- class NoteBook --------------------------
+
+
+class NoteBook:
+
+    @staticmethod
+    def add_note(note, tag, text):
+        file_name = note + ".txt"
+        full_pass = os.path.join(Path().resolve(), "notes", file_name)
+        text_for_note = {
+            "note": note,
+            "tag": tag,
+            "text": "\n"+text,
+            }
+        if not os.path.isfile(full_pass):
+            with open(full_pass, 'w', encoding='utf8') as file:
+                for key, val in text_for_note.items():
+                    file.writelines(f"{key}: {val}\n")
+            return f"Your new note with name '{note}' is created in folder 'notes'"
+        else:
+            return f"Note with name '{note}' is already exist in folder 'notes'"
+
+    @staticmethod
+    def read_note(note):
+        file_name = note + ".txt"
+        full_pass = os.path.join(Path().resolve(), "notes", file_name)
+        note = ""
+        tag = ""
+        text = ""
+
+        if os.path.isfile(full_pass):
+            with open(full_pass, 'r', encoding='utf8') as file:
+                note_data = file.readlines()
+                for index, line in enumerate(note_data):
+                    if index == 0:
+                        note = line.replace('\n', '')
+                    elif index == 1:
+                        tag = line.replace('\n', '')
+                    else:
+                        text += line
+            return f"{note}\n{tag}\n{text.strip()}"
+        return f"Note with name '{note}' was not found in folder 'notes'"
+
+    @staticmethod
+    def update_note(note, tag, text):
+        file_name = note + ".txt"
+        full_pass = os.path.join(Path().resolve(), "notes", file_name)
+
+        if os.path.isfile(full_pass):
+            with open(full_pass, "w", encoding='utf8') as file:
+                file.writelines(f"note: {note}\n")
+                file.writelines(f"{tag}\n")
+                file.writelines(text)
+            return f"Note '{note}' was updated successfully!"
+        else:
+            return f"Note with name '{note}' was not found in folder 'notes'"
+
+    @staticmethod
+    def delete_note(note):
+        file_name = note + ".txt"
+        full_pass = os.path.join(Path().resolve(), "notes", file_name)
+
+        if os.path.isfile(full_pass):
+            os.remove(full_pass)
+            return f"Your note with name '{note}' was deleted from folder 'notes'"
+        else:
+            return f"Note with name '{note}' was not found in folder 'notes'"
+
+    @staticmethod
+    def show_all_notes():
+        folder = os.path.join(Path().resolve(), "notes")
+        notes_list = []
+
+        for filename in os.listdir(folder):
+            notes_list.append(filename)
+
+        if len(notes_list) == 0:
+            return f"Your notebook is still empty.\nPlease add your first note"
+        else:
+            first_string = "Your notebook has the following notes:\n"
+            note_lines = "\n".join(str(record) for record in list(notes_list))
+            return first_string + note_lines
+
+    @staticmethod
+    def find_by_tag(tag):
+        folder = os.path.join(Path().resolve(), "notes")
+        list_of_notes = []
+
+        for filename in os.listdir(folder):
+            note_to_do = NoteBook.read_note(filename.split('.')[0])
+            tags = note_to_do.split('\n')[1].split()
+            if tag in tags:
+                list_of_notes.append(filename)
+
+        if len(list_of_notes) == 0:
+            return f"I can't find any note by this tag.\nPlease enter a valid note's tag for search."
+        else:
+            first_string = "I found the following notes by your tag:\n"
+            note_names = "\n".join(str(record) for record in list(list_of_notes))
+            return first_string + note_names
+
+    @staticmethod
+    def find_by_name(note):
+        folder = os.path.join(Path().resolve(), "notes")
+        list_of_notes = []
+
+        for filename in os.listdir(folder):
+            if note in filename.split('.')[0]:
+                list_of_notes.append(filename)
+
+        if len(list_of_notes) == 0:
+            return f"I can't find any note that contain this name.\nPlease enter a valid note's name for search."
+        else:
+            first_string = "I found  the following notes by searching name:\n"
+            note_names = "\n".join(str(record) for record in list(list_of_notes))
+            return first_string + note_names
+
+
+# -------------------------- class CLINoteBook --------------------------
 
 
 NOTEBOOK = NoteBook()
@@ -283,27 +404,27 @@ You can use the following commands for your NoteBook:
             command = input("Please enter your command: ").lower().strip()
 
             if command == "close":
-                raise SystemExit("\nThank you for using NoteBook.\nSee you later!\n")
+                # raise SystemExit("\nThank you for using NoteBook Bot.\nSee you later!\n")
+                print("\nThank you for using NoteBook Bot.\nSee you later!\n")
+                break
 
             elif command in CLINoteBook.notes_commands_dict.keys():
                 handler = CLINoteBook.notes_commands_dict[command]
                 answer = handler()
                 print(answer)
 
-            # else:
-            #     list_comm = []
-            #     for k in CLINoteBook.notes_commands_dict.keys():
-            #         for item in k.split():
-            #             if command in item:
-            #                 list_comm.append(k)
-            #     if list_comm:
-            #         print('You mean commands: ')
-            #         print(*list_comm,sep=',')
-            #     else:
-            #         print("Incorrect input.\nPlease check and enter correct command (or 'help' or 'close').")
-
             else:
-                print("Incorrect input.\nPlease check and enter correct command (or 'help' or 'close').")
+                list_comm = []
+                for k in CLINoteBook.notes_commands_dict.keys():
+                    for item in k.split():
+                        if command in item:
+                            list_comm.append(k)
+                            break
+                if list_comm:
+                    print('You mean these commands: ')
+                    print(*list_comm, sep=', ')
+                else:
+                    print("Incorrect input.\nPlease check and enter correct command (or 'help' or 'close').")
 
 
 if __name__ == '__main__':
